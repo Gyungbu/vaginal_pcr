216a7c3e9aa0c6ec011a67841d114b13d8b74fc5
Gyungbu <Gyungbu@gmail.com>
2023-08-03 15:21:56 +0900

    Add the code - percentile rank, evaluation

--- vaginal_pcr_analysis.py
+++ vaginal_pcr_analysis.py
@@ -5,6 +5,7 @@ import os, datetime
 import pandas as pd
 import sys
 import numpy as np
+from scipy.stats import percentileofscore, pearsonr
 
 # Check if the script is being called with the correct arguments
 if len(sys.argv) < 2:
@@ -55,14 +56,18 @@ class VaginalPCRAnalysis:
         
         ## Path of Reference files
         curdir = os.path.dirname(os.path.abspath(__file__))
-
+        self.path_db = f"{curdir}/input/db_abundance.csv"
         
         ## Path of output files 
         self.path_harmful = f"{curdir}/output/harmful.csv"
-        self.path_beneficial = f"{curdir}/output/beneficial.csv"        
+        self.path_beneficial = f"{curdir}/output/beneficial.csv"     
+        self.path_percentile_rank_output = f"{curdir}/output/percentile_rank.csv"
+        self.path_eval_output = f"{curdir}/output/eval.csv"
         
         ## Dataframe of Reference files
         self.df_exp = None
+        self.df_abundance = None
+        self.df_db = None
         
         ## Dataframe of output files to calculate
         self.df_harmful = None
@@ -70,7 +75,7 @@ class VaginalPCRAnalysis:
         
         ## Lists used for calculation
         self.li_new_sample_name = None
-        
+        self.li_microbiome = None
         
         
     def ReadDB(self):
@@ -81,6 +86,8 @@ class VaginalPCRAnalysis:
         rvmsg = "Success"
         
         try:           
+            self.df_db = pd.read_csv(self.path_db, index_col=0)
+            
             self.df_exp = pd.read_excel(self.path_exp)
             idx_well_id = list(self.df_exp[self.df_exp.columns[0]]).index('Well')
             self.df_exp = self.df_exp[idx_well_id:].rename(columns = self.df_exp[idx_well_id:].reset_index().iloc[0]).drop(idx_well_id).set_index('Well')
@@ -102,7 +109,6 @@ class VaginalPCRAnalysis:
             idx_nan_id = self.df_exp.index.to_list().index(np.nan)
             self.df_exp = self.df_exp.iloc[:idx_nan_id, :]
             self.df_exp.loc[self.df_exp['Ct'] == 'Undetermined', 'Ct'] = 40.1
-            print(self.df_exp)
                   
         except Exception as e:
             print(str(e))
@@ -121,27 +127,134 @@ class VaginalPCRAnalysis:
         
         try:      
             self.li_new_sample_name = list(dict.fromkeys(self.df_exp.sample_name)) 
+            self.li_microbiome = ['L_crispatus', 'L_gasseri', 'L_iners', 'L_jensenii', 'G_vaginalis', 'F_vaginae', 'BVAB-1']
             
-            self.df_exp['RA'] = 0
+            self.df_abundance = pd.DataFrame(index = self.li_microbiome, columns = self.li_new_sample_name)
+            self.df_abundance = self.df_abundance.fillna(0)
             
-            for idx_exp, row_exp in self.df_exp.iterrows():
-                
-                sample_name = row_exp['sample_name']
-                
-                condition = (self.df_exp.sample_name == sample_name) & (self.df_exp.microbiome == 'Universal')
-                
-                ct = row_exp['Ct']
-                ct_universal = self.df_exp[condition]['Ct'].values[0] 
-                
-                self.df_exp.loc[idx_exp, 'RA'] = 2**(-(ct- ct_universal))
-            print(self.df_exp)
+            for i in range(len(self.li_new_sample_name)):
+                for j in range(len(self.li_microbiome)):            
+            
+                    sample_name = self.li_new_sample_name[i]
+
+                    condition_universal = (self.df_exp.sample_name == sample_name) & (self.df_exp.microbiome == 'Universal')
+                    condition_microbiome = (self.df_exp.sample_name == sample_name) & (self.df_exp.microbiome == self.li_microbiome[j])
+
+                    ct = self.df_exp[condition_microbiome]['Ct'].values[0]
+                    ct_universal = self.df_exp[condition_universal]['Ct'].values[0]   
+                    
+                    self.df_abundance.loc[self.li_microbiome[j], self.li_new_sample_name[i]] = 2**(-(ct- ct_universal))
+            
+            print(self.df_abundance)
+                        
             
         except Exception as e:
             print(str(e))
             rv = False
             rvmsg = str(e)
             print(f"Error has occurred in the {myNAME} process") 
+
+    def CalculatePercentileRank(self):
+        """
+        Calculate the Percentile Rank and Save the Percentile Rank data as an Csv file.
+
+        Returns:
+        A tuple (success, message), where success is a boolean indicating whether the operation was successful,
+        and message is a string containing a success or error message.
+        """          
+        myNAME = self.__class__.__name__+"::"+sys._getframe().f_code.co_name
+        WriteLog(myNAME, "In", type='INFO', fplog=self.__fplog)
+         
+        rv = True
+        rvmsg = "Success"
+        
+        try:      
+
+            # Create an empty data frame 
+            self.df_percentile_rank = pd.DataFrame(index = self.li_new_sample_name, columns = self.li_microbiome)
+                              
+            # Loop through all samples and phenotypes and calculate the percentile rank
+            for i in range(len(self.li_new_sample_name)):
+                for j in range(len(self.li_microbiome)):
+                    self.df_percentile_rank.loc[self.li_new_sample_name[i], self.li_microbiome[j]] = (percentileofscore(list(self.df_db.loc[self.li_microbiome[j]]), self.df_abundance.loc[self.li_microbiome[j], self.li_new_sample_name[i]], kind='mean')).round(1)
             
+
+            # Outliers
+            # Replace percentile ranks that are less than or equal to 5 with 5, and those that are greater than or equal to 95 with 95
+            for i in range(len(self.li_microbiome)):
+                self.df_percentile_rank.loc[self.df_percentile_rank[self.li_microbiome[i]]<=5, self.li_microbiome[i]] = 5.0
+                self.df_percentile_rank.loc[self.df_percentile_rank[self.li_microbiome[i]]>=95, self.li_microbiome[i]] = 95.0      
+                     
+            # Replace missing values with the string 'None'    
+            self.df_percentile_rank = self.df_percentile_rank.fillna('None')
+
+            # Save the output file - Percentile Rank of the samples
+            self.df_percentile_rank.to_csv(self.path_percentile_rank_output, encoding="utf-8-sig", index_label='serial_number')
+            
+        except Exception as e:
+            print(str(e))
+            rv = False
+            rvmsg = str(e)
+            print(f"Error has occurred in the {myNAME} process")    
+            sys.exit()
+    
+        return rv, rvmsg    
+    
+    def EvaluatePercentileRank(self):
+        """
+        Evaluate based on percentile rank value and Save the Evaluation data as an Csv file
+
+        Returns:
+        A tuple (success, message), where success is a boolean indicating whether the operation was successful,
+        and message is a string containing a success or error message.
+        """          
+        myNAME = self.__class__.__name__+"::"+sys._getframe().f_code.co_name
+        WriteLog(myNAME, "In", type='INFO', fplog=self.__fplog)
+         
+        rv = True
+        rvmsg = "Success"
+        
+        try:                 
+            self.df_eval = pd.DataFrame(index=self.df_percentile_rank.index)
+            
+            li_positive_var = ['L_crispatus', 'L_gasseri', 'L_iners', 'L_jensenii']
+            
+            for col in self.df_percentile_rank:
+                if col in li_positive_var:
+                    # Define the conditions and corresponding values
+                    conditions = [
+                        self.df_percentile_rank[col] >= 70,
+                        (self.df_percentile_rank[col] >= 30) & (self.df_percentile_rank[col] < 70),
+                        self.df_percentile_rank[col] <= 30
+                    ]
+                    
+                    values = ['좋음', '보통', '나쁨']     
+                    
+                    self.df_eval[col] = np.select(conditions, values)  
+                                 
+                else: # MRS, DysbiosisHarmful
+                    # Define the conditions and corresponding values
+                    conditions = [
+                        self.df_percentile_rank[col] >= 70,
+                        (self.df_percentile_rank[col] >= 30) & (self.df_percentile_rank[col] < 70),
+                        self.df_percentile_rank[col] <= 30
+                    ]
+                    
+                    values = ['나쁨', '보통', '좋음']     
+                    
+                    self.df_eval[col] = np.select(conditions, values)       
+                    
+            # Save the output file - df_eval
+            self.df_eval.to_csv(self.path_eval_output, encoding="utf-8-sig", index_label='serial_number') 
+
+        except Exception as e:
+            print(str(e))
+            rv = False
+            rvmsg = str(e)
+            print(f"Error has occurred in the {myNAME} process")    
+            sys.exit()
+    
+        return rv, rvmsg         
 ####################################
 # main
 ####################################
@@ -150,5 +263,7 @@ if __name__ == '__main__':
     vaginalpcranalysis = VaginalPCRAnalysis(path_exp)
     vaginalpcranalysis.ReadDB()      
     vaginalpcranalysis.CalculateProportion()  
+    vaginalpcranalysis.CalculatePercentileRank()   
+    vaginalpcranalysis.EvaluatePercentileRank()     
     
     print('Analysis Complete')
